30f106c407c4e30bba30cec438818fed50e65ba0
donghuan1 <donghuan1@staff.weibo.com>
2017-12-29 16:59:52 +0800

    productInfo redis

--- productInfo.php
+++ productInfo.php
@@ -1,28 +1,33 @@
 <?php
-
-$client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
-  
-$session = $client->login('mobile', 'mobile');
-
 $ids=$_GET["ids"];
 $lists = explode('-', $ids);
 $result = array();
-foreach ($lists as $id){ 
-	// print_r($sku);
-	// $sku = '6629452600183';
-	$args = array(
-    'productId' => $id,
-    'storeView' => 'china'
-	);
-	$res = $client->call($session, 'catalog_product.info', $args);
-	array_push($result, $res);
-	// array_push($result, $client->call($session, 'catalog_product.info', $sku));
+
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+
+$redis_key = 'productInfo'.$ids;
+$redis_result = $redis->get($redis_key);
+if ($redis_result) {
+	echo $redis_result;
+}else{
+	$client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
+	$session = $client->login('mobile', 'mobile');
+	foreach ($lists as $id){ 
+		$args = array(
+	    'productId' => $id,
+	    'storeView' => 'china'
+		);
+		$res = $client->call($session, 'catalog_product.info', $args);
+		
+		array_push($result, $res);
+	}
+	$client->endSession($session);
+	$redis->set($redis_key,json_encode($result));
+	echo json_encode($result);
 }
-// $result = $client->call($session, 'catalog_product.info', '5006');
 
-// var_dump($result);
-echo json_encode($result);
 
-// If you don't need the session anymore
-//$client->endSession($session);
+
+
 ?>
