d13a6272988def9630d076d911ecb0f204559d8b
ldhlfzysys <ldhlfzysys@163.com>
2017-12-30 20:41:28 +0800

    categoryTree redis

--- categoryTree.php
+++ categoryTree.php
@@ -1,16 +1,24 @@
 <?php
 
-$client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
-  
-$session = $client->login('mobile', 'mobile');
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+
+$redis_key = 'productTreeAll';
+$redis_result = $redis->get($redis_key);
+
+if ($redis_result) {
+	echo $redis_result;
+
+}else{
+	$client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl'); 
+	$session = $client->login('mobile', 'mobile');
+	$result = $client->call($session, 'catalog_category.tree');
+	$client->endSession($session);
+	$json_result = json_encode($result);
+	$redis->set($redis_key,$json_result);
+	echo $json_result;
+
+}
 
 
-$result = $client->call($session, 'catalog_category.tree');
-
-// var_dump($result);
-// print_r($result);
-echo json_encode($result);
-
-// If you don't need the session anymore
-//$client->endSession($session);
 ?>
