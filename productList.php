9e34c4a10f1b35927567fa90e6fe6d60958ba92c
ldhlfzysys <ldhlfzysys@163.com>
2017-12-30 20:04:33 +0800

    productlis redis

--- productList.php
+++ productList.php
@@ -1,15 +1,22 @@
 <?php
 
-$client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
-  
-$session = $client->login('mobile', 'mobile');
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+
+$redis_key = 'productlistall';
+$redis_result = $redis->get($redis_key);
+
+if ($redis_result) {
+	echo $redis_result;
+}else{
+	$client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
+	$session = $client->login('mobile', 'mobile');
+	$result = $client->call($session, 'catalog_product.list');
+	$client->endSession($session);
+	$json_result = json_encode($result);
+	$redis->set($redis_key,$json_result);
+	echo $json_result;
+}
 
 
-$result = $client->call($session, 'catalog_product.list');
-
-// var_dump($result);
-echo json_encode($result);
-
-// If you don't need the session anymore
-//$client->endSession($session);
 ?>
