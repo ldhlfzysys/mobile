2bc8e8a32faaabfddcff6ebcbbfb272db7ef2db2
jingyu11 <jingyu11@staff.weibo.com.cn>
2018-01-02 22:55:03 +0800

    登陆流程

--- login.php
+++ login.php
@@ -1,11 +1,9 @@
 <?php
 
-
-#传入商品id和数量，数量为整形，商品id为字符串
-
+function login($userid,$password){
 	$client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
 	$session = $client->login('mobile', 'mobile');
-	$params = array('email'=>'411455886@qq.com', 'password'=>'11180x119');
+ 	$params = array('email'=>$userid, 'password'=>$password);
  	
  	try {
 
@@ -15,5 +13,20 @@
 	    echo "==> Error: ".$e->getMessage();
 	}
 	echo $result;
+}
 
+// $result = login("wudishentu239@163.com","insomnia0903");
+// $obj = json_decode($result,true);
+// var_dump(json_decode($result,true));
+// if ($obj['status'] == 'OK') {
+// 	$userid = $obj['userData']['customer_id'];
+// 	setcookie('bdb-ui',$userid);
+// }
+
+if (isset($_POST['userid']) && isset($_POST['password'])) {
+	$userid = $_POST['userid'];
+	$password = $_POST['password'];
+	$result = login($userid,$password);
+	echo $result;
+}
 ?>
