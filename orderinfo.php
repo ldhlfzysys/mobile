98dddbea3a36dab2fc4c112b239583bd93324949
jingyu11 <jingyu11@staff.weibo.com.cn>
2018-01-14 17:12:47 +0800

    商品详情页修改

---
+++ orderinfo.php
@@ -0,0 +1,36 @@
+<?php
+ini_set('display_errors',1); 
+ini_set('display_startup_errors',1);
+function getOrderInfo($orderid){
+
+	$redis = new Redis();
+	$redis->connect('127.0.0.1', 6379);
+
+	$redis_key = 'order_'.$orderid;
+	$redis_result = $redis->get($redis_key);
+	$result;
+	if ($redis_result) {
+		$result = $redis_result;
+	}else
+	{
+		$client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
+		$session = $client->login('mobile', 'mobile');
+		$result = $client->call($session, 'sales_order.info',$orderid);
+	}
+
+	return $result;
+}
+
+if (isset($_GET['orderid'])) {
+	$result = getOrderInfo($_GET['orderid']);
+	$json_result = json_encode($result);
+	echo $json_result;
+}else
+{
+	// echo '<script type="text/javascript">';
+	// echo 'window.location.href="http://bdbbuy.com/mobile/login.html";';
+	// echo '</script>';
+}
+
+
+?>
