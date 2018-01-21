2fcc4643fd1b697c31aa0b380e3d41dffa79cd93
ldhlfzysys <ldhlfzysys@163.com>
2018-01-21 17:07:56 +0800

    搜索优化

--- searchProduct.php
+++ searchProduct.php
@@ -1,13 +1,16 @@
 <?php
 
 $searchword=$_GET['s'];
+$arr = mb_str_split($searchword);
+$arr_str = join("%",$arr);
+$regx = "%".$arr_str."%";
 
 $client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
   
 $session = $client->login('mobile', 'mobile');
 
 $complexFilter = array(
-            'name' => array('like' => '%'.$searchword.'%')
+            'name' => array('like' => $regx)
 );
 
 $args = array(
@@ -19,6 +22,10 @@ $result = $client->call($session,'catalog_product.list',$args);
 
 echo json_encode($result);
 
+
+function mb_str_split($str){  
+    return preg_split('/(?<!^)(?!$)/u', $str );  
+} 
 
 
 ?>
--- test.php
+++ test.php
@@ -2,14 +2,21 @@
 ini_set('display_errors',1); 
 ini_set('display_startup_errors',1);
 
-$searchword=$_GET['s'];
+// $searchword=$_GET['s'];
 
 $client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
   
 $session = $client->login('mobile', 'mobile');
 
+$xx="黑力";
+$arr = mb_str_split($xx);
+$regx = join("%",$arr);
+$dd = "%".$regx."%";
+
+
+
 $complexFilter = array(
-            'name' => array('like' => '%'.$searchword.'%')
+            'name' => array('like' => $dd)
 );
 
 $args = array(
@@ -20,7 +27,9 @@ $args = array(
 $result = $client->call($session,'catalog_product.list',$args);
 
 var_dump($result);
-
+function mb_str_split($str){  
+    return preg_split('/(?<!^)(?!$)/u', $str );  
+}  
 
 
 ?>
