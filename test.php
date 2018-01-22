1f6c2df372c4728e8c72669ce2f98a8ac24fe98b
ldhlfzysys <ldhlfzysys@163.com>
2018-01-22 21:46:59 +0800

    商品特价

--- test.php
+++ test.php
@@ -2,34 +2,14 @@
 ini_set('display_errors',1); 
 ini_set('display_startup_errors',1);
 
-// $searchword=$_GET['s'];
+$searchword=$_GET['s'];
 
 $client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
   
 $session = $client->login('mobile', 'mobile');
 
-$xx="黑力";
-$arr = mb_str_split($xx);
-$regx = join("%",$arr);
-$dd = "%".$regx."%";
-
-
-
-$complexFilter = array(
-            'name' => array('like' => $dd)
-);
-
-$args = array(
-	'filters' => $complexFilter,
-	'storeView' => '16'
-);
-
-$result = $client->call($session,'catalog_product.list',$args);
-
+$result = $client->call($session, 'catalog_product_attribute_tier_price.info', $searchword);
 var_dump($result);
-function mb_str_split($str){  
-    return preg_split('/(?<!^)(?!$)/u', $str );  
-}  
 
 
 ?>
