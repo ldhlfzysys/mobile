43d28bb46c46f227b81005d61c5d8775842f6509
ldhlfzysys <ldhlfzysys@163.com>
2018-01-24 22:15:45 +0800

    图片地址更新

--- res/js/ui-me.js
+++ res/js/ui-me.js
@@ -1,3 +1,19 @@
+function basename(str1)
+{
+   str2="/"
+   
+   var s = str1.lastIndexOf(str2);
+   if (s==-1) {
+   str2="\\"
+   var s = str1.lastIndexOf(str2);
+   }
+   if (s==-1) alert("字符串非法")
+   else{
+    return(str1.substring(s+1,str1.length));
+   }
+   return ""
+}
+
 $(function () {
     //绑定滚动条事件
     $(window).bind("scroll", function () {
@@ -34,7 +50,7 @@ $(function () {
 			var id = res['product_id'];
 			var name = res['name'];
 			var price = res['price'];
-			var url = 'http://bdbbuy.com/media/catalog/product' + res['image'];
+			var url = 'http://bdbbuy-mobile.oss-cn-hongkong.aliyuncs.com/' + basename(res['image']) +'?x-oss-process=style/center';
 			var str = '<a href="ui-product.phtml?productId='+id+'" class="aui-list-product-item"><div class="aui-list-product-item-img"><img src="' + url + '" alt=""></div><div class="aui-list-product-item-text"><h3>' + name + '</h3><div class="aui-list-product-mes-box"><div><span class="aui-list-product-item-price"><em>CA$</em>' + price.substr(0,price.indexOf(".")+3) + '</span></div></div></div></a>';
 			//<span class="aui-list-product-item-del-price"><em>$</em>' + special_price + '</span>
 			$("#recommend").append(str);
