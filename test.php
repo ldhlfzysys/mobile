e97327b603091c7aeb721281ede254523755f9a1
ldhlfzysys <ldhlfzysys@163.com>
2018-01-27 15:18:18 +0800

    跳转登陆页逻辑修改

--- test.php
+++ test.php
@@ -1,30 +1,11 @@
 <?php
-$searchword=$_GET['s'];
-$arr = mb_str_split($searchword);
-$arr_str = join("%",$arr);
-$regx = "%".$arr_str."%";
+
 
 $client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
   
 $session = $client->login('mobile', 'mobile');
 
-$complexFilter = array(
-			'name' => array('like' => $regx),
-            'meta_keyword' => array('like' => $regx)
-);
+$shipping_list = $client->call($session,'cart_shipping.list','611');
 
-$args = array(
-	'filters' => $complexFilter,
-	'storeView' => '16'
-);
-
-$result = $client->call($session,'catalog_product.list',$args);
-
-echo json_encode($result);
-
-
-function mb_str_split($str){  
-    return preg_split('/(?<!^)(?!$)/u', $str );  
-} 
-
+var_dump($shipping_list);
 ?>
