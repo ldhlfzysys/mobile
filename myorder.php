d709eeeb498013412162e379d3b88ad437c74615
xl <93656715@qq.com>
2018-02-02 21:30:06 +0800

    许雷，订单获取按照下单时间降序排序；查看全部；

--- myorder.php
+++ myorder.php
@@ -1,6 +1,25 @@
 <?php
 ini_set('display_errors',1); 
 ini_set('display_startup_errors',1);
+
+// 按照订单创建时间，降序
+function descByTime($orderList){
+	$sort = array(
+	    'direction' => 'SORT_DESC', //排序顺序标志 SORT_DESC 降序；SORT_ASC 升序
+	    'field'     => 'created_at',       //排序字段
+	 );
+	$arrSort = array();
+	foreach($orderList AS $uniqid => $row){
+	    foreach($row AS $key=>$value){
+	        $arrSort[$key][$uniqid] = $value;
+	    }
+	}
+	if($sort['direction']){
+	    array_multisort($arrSort[$sort['field']], constant($sort['direction']), $orderList);
+	}
+	return $orderList;
+}
+
 function getOrderList($userid){
 
 	$redis = new Redis();
@@ -11,13 +30,14 @@ function getOrderList($userid){
 	$result;
 	if ($redis_result) {
 		$result = $redis_result;
-	}else
-	{
+	}else{
 		$client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
 		$session = $client->login('mobile', 'mobile');
 		$result = $client->call($session, 'order.list');
 	}
 
+	// 排序
+	$result = descByTime($result);
 
 	$myOrderList = array();
 	foreach ($result as $key => $value) {
--- test.php
+++ test.php
@@ -1,22 +1,56 @@
 <?php
 
-$client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
+// $client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
   
-$session = $client->login('mobile', 'mobile');
+// $session = $client->login('mobile', 'mobile');
 
   
 
-$complexFilter = array(
-            'status' => 1
+// $complexFilter = array(
+//             'status' => 1
+// );
+
+// $args = array(
+// 	'filters' => $complexFilter,
+// 	'storeView' => '16'
+// );
+
+// $result = $client->call($session,'catalog_product.list',$args);
+
+
+// echo json_encode($result);
+
+function descTime($orderList){
+	$sort = array(
+	    'direction' => 'SORT_DESC', //排序顺序标志 SORT_DESC 降序；SORT_ASC 升序
+	    'field'     => 'time',       //排序字段
+	 );
+	  $arrSort = array();
+	 foreach($orderList AS $uniqid => $row){
+	     foreach($row AS $key=>$value){
+	         $arrSort[$key][$uniqid] = $value;
+	     }
+	 }
+	 if($sort['direction']){
+	     array_multisort($arrSort[$sort['field']], constant($sort['direction']), $orderList);
+	 }
+
+	return $orderList;
+}
+
+$orderList = array(
+	array(
+		"time" =>"2018-02-02 21:18:42",
+	),
+	array(
+		"time" =>"2018-02-01 21:18:42",
+	),
+	array(
+		"time" =>"2018-02-03 21:18:42",
+	),
 );
 
-$args = array(
-	'filters' => $complexFilter,
-	'storeView' => '16'
-);
-
-$result = $client->call($session,'catalog_product.list',$args);
 
 
-echo json_encode($result);
+ var_dump($orderList)
 ?>
