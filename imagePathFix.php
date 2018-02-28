fdf1bdf8b16e44ada0b0e79010c6b012f5233106
xl <93656715@qq.com>
2018-02-28 23:06:36 +0800

    许雷，修改OSS文件为HTTPS

--- imagePathFix.php
+++ imagePathFix.php
@@ -3,13 +3,13 @@
 function smallImage($originPath){
 	$imageName = basename($originPath);
 	$imageNameFix = preg_replace("/_[\d]/", "", $imageName);
-	return 'http://bdbbuy-mobile.oss-cn-hongkong.aliyuncs.com/'.$imageNameFix.'?x-oss-process=style/center';
+	return 'https://bdbbuy-mobile.oss-cn-hongkong.aliyuncs.com/'.$imageNameFix.'?x-oss-process=style/center';
 }
 
 function originImage($originPath){
 	$imageName = basename($originPath);
 	$imageNameFix = preg_replace("/_[\d]/", "", $imageName);
-	return 'http://bdbbuy-mobile.oss-cn-hongkong.aliyuncs.com/'.$imageNameFix;
+	return 'https://bdbbuy-mobile.oss-cn-hongkong.aliyuncs.com/'.$imageNameFix;
 }
 
 
