a04ded2ab0f5eb09ee9bb67e820ac0043e31d884
ldhlfzysys <ldhlfzysys@163.com>
2018-03-01 20:15:41 +0800

    通过用户获取购物车示例

--- test.php
+++ test.php
@@ -1,44 +1,20 @@
 <?php
-ini_set('display_errors',1); 
-require __DIR__  . '/PayPal-PHP-SDK/autoload.php';
-use PayPal\Api\Payment;
-use PayPal\Api\PaymentExecution;
 
-if(isset($_GET['paymentId']) && isset($_GET['token']) && isset($_GET['PayerID']) )
-{
-	$paymentId = $_GET['paymentId'];
-	$payerId = $_GET['PayerID'];
+    $client = new SoapClient('http://bdbbuy.com/index.php/api/soap/?wsdl');  
+    $session = $client->login('mobile', 'mobile');
 
-    $apiContext = new \PayPal\Rest\ApiContext(
-        new \PayPal\Auth\OAuthTokenCredential(
-            'AX5s9PVAnhgl6X24ygTorm4jRgsZeE19LkzUiNSCK8jPGGdkAVAG8EhT-Fetr8Thxk90FThHWCT2ejI5',     // ClientID
-            'EM4od7UoQY02yTTobXDmnZEHuJnvCbwFwPBHkT53MT_nJgJ80AvXrZY5t2JH-dSfeyG6NK7jyUB7SByD'      // ClientSecret
-        )
-    );
-    $apiContext->setConfig(
-        array(
-            'mode' => 'live',
-        )
-    );
-
-    $payment = Payment::get($paymentId, $apiContext);
-    $execution = new PaymentExecution();
-    $execution->setPayerId($payerId);
-
-    $transaction = $payment->getTransactions();
-
-    $execution->addTransaction($transaction);
-
-    // var_dump($execution);
+    $params = array('customerid'=>'4');
+        $args = array(
+        'store' => '16'
+        );
     try {
-         $result = $payment->execute($execution, $apiContext);
-    } catch (Exception $ex) {
-    	var_dump($ex);
-           // ResultPrinter::printError("Get Payment", "Payment", null, null, $ex);
-        exit(1);
+
+        // $result = $client->call($session, 'cart.quotebyuserid', $params);
+        $result = $client->call($session, 'cart.info','268', $args);
+    }
+    catch (Exception $e) { //while an error has occured
+        echo "==> Error: ".$e->getMessage();
     }
     var_dump($result);
-
-}
 
 ?>
